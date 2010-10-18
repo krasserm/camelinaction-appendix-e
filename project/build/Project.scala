10f7ab75d5d0bddee4538ae3fbc7152553fb8d33
Martin Krasser <krasserm@googlemail.com>
2010-10-18 16:51:40 +0200

    Upgraded to Camel 2.5 release candidate

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,15 +3,15 @@ import sbt._
 class Project(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
   val akkaHome = Path.fromFile(System.getenv("AKKA_HOME"))
   val embeddedRepo = MavenRepository("eer",  (akkaHome / "embedded-repo").asURL.toString)
-  val apacheSnapshots = MavenRepository("apache-snapshots", "https://repository.apache.org/content/repositories/snapshots/")
+  val camelStaging = MavenRepository("camel-staging", "https://repository.apache.org/content/repositories/orgapachecamel-001/")
 
   override val sjsonModuleConfig       = ModuleConfiguration("sjson.json", embeddedRepo)
 
   val akkaCamel = akkaModule("camel")
   val akkaKernel = akkaModule("spring")
 
-  val camelJetty = "org.apache.camel" % "camel-jetty" % "2.5-SNAPSHOT" % "compile"
-  val camelSpring = "org.apache.camel" % "camel-spring" % "2.5-SNAPSHOT" % "compile"
+  val camelJetty = "org.apache.camel" % "camel-jetty" % "2.5.0" % "compile"
+  val camelSpring = "org.apache.camel" % "camel-spring" % "2.5.0" % "compile"
   val log4jOverSlf4j = "org.slf4j" % "log4j-over-slf4j" % "1.6.1" % "runtime"
 
   // Temporary workaround to ignore logback.xml in akka-core_2.8.0-0.10.jar
