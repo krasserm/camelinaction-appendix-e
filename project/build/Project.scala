2c4819a98b0fa136cc86ab49a39d7f5f49a1e0e4
Martin Krasser <krasserm@googlemail.com>
2012-02-01 07:24:20 +0100

    Upgrade to Akka 1.3 and Camel 2.9.0

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -6,9 +6,9 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
   val akkaCamel = akkaModule("camel")
   val akkaKernel = akkaModule("spring")
 
-  val camelCore = "org.apache.camel" % "camel-core" % "2.8.1" % "compile"
-  val camelJetty = "org.apache.camel" % "camel-jetty" % "2.8.1" % "compile"
-  val camelSpring = "org.apache.camel" % "camel-spring" % "2.8.1" % "compile"
-  val camelStream = "org.apache.camel" % "camel-stream" % "2.8.1" % "compile"
+  val camelCore = "org.apache.camel" % "camel-core" % "2.9.0" % "compile"
+  val camelJetty = "org.apache.camel" % "camel-jetty" % "2.9.0" % "compile"
+  val camelSpring = "org.apache.camel" % "camel-spring" % "2.9.0" % "compile"
+  val camelStream = "org.apache.camel" % "camel-stream" % "2.9.0" % "compile"
   val log4jOverSlf4j = "org.slf4j" % "log4j-over-slf4j" % "1.6.1" % "runtime"
 }
